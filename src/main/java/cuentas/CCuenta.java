1996cd222fb77dbd31c31c16422a42935a93e36a
martin <gagochorenmartin@gmail.com>
2023-03-14 09:29:52 +0100

    generado el javadoc para la clase ccuenta

--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -1,16 +1,42 @@
 package cuentas;
 
+/**
+ * Clase que representa una CuentaBancaria
+ * @author Martín Gago 
+ */
 public class CCuenta {
 
+    /**
+     * El nombre del propietario de la cuenta
+     */
     private String nombre;
+    /**
+     * String con el numero de cuenta
+     */
     private String cuenta;
+    /**
+     * Valor double saldo de la cuenta
+     */
     private double saldo;
+    /**
+     * valor double con el tipo de interés
+     */
     private double tipoInterés;
 
+    /**
+     *
+     */
     public CCuenta()
     {
     }
 
+    /**
+     *
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -18,11 +44,20 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     *
+     * @return
+     */
     public double estado()
     {
         return saldo;
     }
 
+    /**
+     * Ingresa una cantidad de dinero en la cuenta
+     * @param cantidad valor double de dinero que se va a ingresar en la cuenta
+     * @throws Exception Error que devuelve si la cantidad a ingresar es negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -30,6 +65,11 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     * Retira una cantidad de dinero de la cuenta
+     * @param cantidad valor double que retira dinero de una cuenta Bancaria
+     * @throws Exception Devuelve un error en caso de que se inserte cantidad negativa o superior al saldo actual
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -39,35 +79,68 @@ public class CCuenta {
         saldo = saldo - cantidad;
     }
 
-    //Getters and setters
+    //Getters and 
+
+    /**
+     *
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     *
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getCuenta() {
         return cuenta;
     }
 
+    /**
+     *
+     * @param cuenta
+     */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getSaldo() {
         return saldo;
     }
 
+    /**
+     *
+     * @param saldo
+     */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
+    /**
+     *
+     * @param tipoInterés
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
